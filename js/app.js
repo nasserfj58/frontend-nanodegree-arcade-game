c49a23f2310d122452d256c38fa754a41ca4ef71
Nasser <nasserfj58@gmail.com>
2017-09-08 18:38:52 +0300

    fix rendring the objects

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+	//var obj=Object.create(Enemy.prototype);
     this.sprite = 'images/enemy-bug.png';
+	this.x=x;
+	this.y=y;
+	this.speed=(Math.random());
+	
+	//return obj;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+	this.location=this.location*this.speed*dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +31,53 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x,y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+	this.location=0;
+	this.speed=0;
+	this.x=x;
+	this.y=y;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+	this.location=this.location*this.speed*dt;
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(allowedKeys){
+	
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = new Player(400,50);
 
+
+var allEnemies = [];
+function putEnemies () {
+  allEnemies.push(new Enemy(-2, 60));
+  //allEnemies.push(new Enemy(-2, 100));
+  allEnemies.push(new Enemy(-2,140));
+  allEnemies.push(new Enemy(-2,230));
+};
+
+putEnemies();
 
 
 // This listens for key presses and sends the keys to your
@@ -42,5 +90,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    Player.handleInput(allowedKeys[e.keyCode]);
 });
